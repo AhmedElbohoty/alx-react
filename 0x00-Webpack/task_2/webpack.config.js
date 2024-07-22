8c9e41114c05ae9b4edcd7aa5cfcfc78d092e143
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-07-22 23:59:55 +0300

    0x00-Webpack - task 3

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -2,13 +2,19 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
+
   entry: {
     main: path.resolve(__dirname, './js/dashboard_main.js'),
   },
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
   },
+
+  performance: {
+    maxAssetSize: 1000000,
+  },
+
   module: {
     rules: [
       {
@@ -16,14 +22,18 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
-        type: 'asset/resource',
+        test: /\.(gif|png|jp?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
       },
     ],
-  },
-  performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000,
   },
 };
