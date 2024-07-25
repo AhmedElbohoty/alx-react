a4e9e5f07791e27f8cc81ee773b0b37210b84f4b
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-07-25 03:41:32 +0300

    0x01-react-intro - task 0 and task 1

---
+++ 0x00-react_introduction/task_0/dashboard/src/App.js
@@ -0,0 +1,27 @@
+import logo from './assets/holberton_logo.jpg';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* App header */}
+      <div className="App-header">
+        <img className="App-header-img" src={logo} alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+
+      {/* App body */}
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+
+      {/* App footer */}
+      <div className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
